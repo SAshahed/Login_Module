910a7b2ebc946f8126e2c784c0bc7efed7794d8a
Shahed-Alam <shrmbd@gmail.com>
2024-06-23 14:50:26 +0600

    login system updated

---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+
+Route::get('/', [AuthController::class, 'login']) ;
+Route::post('login', [AuthController::class, 'AuthLogin']);
+Route::get('logout', [AuthController::class, 'logout']);
+Route::get('forgot-password', [AuthController::class, 'forgotpassword']);
+Route::post('forgot-password', [AuthController::class, 'PostForgotPassword']);
+Route::get('reset/{token}', [AuthController::class, 'reset']);
+Route::post('reset/{token}', [AuthController::class, 'PostReset']);
+
+
+  
+
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
+});
+
+
+Route::group(['middleware' => 'admin'], function () {
+
+    Route::get('admin/dashboard', [DashboardController::class, 'dashboard']);
+    
+});
+
+Route::group(['middleware' => 'teacher'], function () {
+
+    Route::get('teacher/dashboard', [DashboardController::class, 'dashboard']);
+
+
+});
+Route::group(['middleware' => 'student'], function () {
+    
+    Route::get('student/dashboard', [DashboardController::class, 'dashboard']);
+
+
+});
+Route::group(['middleware' => 'parent'], function () {
+    Route::get('parent/dashboard', [DashboardController::class, 'dashboard']);
+
+
+});
+
